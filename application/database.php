8b78db970bbf7dfd813a97edb4eb0591c2a767d7
zzw <zuozhiwei0@gmail.com>
2018-08-12 12:12:39 +0800

    1

--- application/database.php
+++ application/database.php
@@ -13,11 +13,11 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '182.254.132.179',
+    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'hgo_cooperation',
+    'database'        => '',
     // 用户名
-    'username'        => 'guest',
+    'username'        => 'root',
     // 密码
     'password'        => '12345678',
     // 端口
