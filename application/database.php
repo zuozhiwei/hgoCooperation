40d77257d4130e0741e29cbcf814a56e042efc66
zzw <zuozhiwei0@gmail.com>
2018-08-12 12:05:38 +0800

    add login

--- application/database.php
+++ application/database.php
@@ -13,15 +13,15 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '182.254.132.179',
     // 数据库名
-    'database'        => '',
+    'database'        => 'hgo_cooperation',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'guest',
     // 密码
-    'password'        => '',
+    'password'        => '12345678',
     // 端口
-    'hostport'        => '',
+    'hostport'        => '3306',
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
