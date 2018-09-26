5adea1a505d3bd0e764cbf4ba4a1e3ec4245143f
zzw <zuozhiwei0@gmail.com>
2018-09-26 11:13:40 +0800

    任务列表

--- application/coop/controller/Task.php
+++ application/coop/controller/Task.php
@@ -13,39 +13,47 @@ class Task extends Controller
      */
     public function index()
     {
-        # 查询条件
-        $where = array(
+//         # 查询条件
+//         $where = array(
 
-        );
-        # 获取任务列表和用户列表
-        $taskList = TaskModel::all($where);
-        $userList = User::where()->field("id,name");
-        # 循环处理
-        foreach ($taskList as &$task) {
-            $coNames = array();
-            $leaderInfo = "";
-            ## 协助人id
-            $coIds = explode(",",$task["co_ids"]);
-            ## 负责人id
-            $leaderId = $task["leader_id"];
-            ## 负责人姓名
-            foreach ($userList as $user) {
-                if ($leaderId == $user["id"]) {
-                    $leaderInfo = $user;
-                }
-            }
-            ## 协助人姓名
-            foreach ($coIds as $coId) {
-                foreach ($userList as $user) {
-                    if ($coId == $user["id"]) {
-                        $coNames[] = $user;
-                    }
-                }
-            }
-            $task["coNames"] = $coNames;
-            $task["leaderInfo"] = $leaderInfo;
-        }
-        $this->assign("taskList",$taskList);
+//         );
+//         # 获取任务列表和用户列表
+//         $taskList = TaskModel::all($where)->toArray();
+//         $userList = User::where("id,name")->field("id,name")->toArray();
+//         # 循环处理
+// //        foreach ($taskList as &$task) {
+// //            $task = $task["data"];
+// //        }
+//         foreach ($taskList as &$task) {
+//             $coNames = array();
+//             $leaderInfo = "";
+//             ## 协助人id
+//             $coIds = explode(",",$task["co_ids"]);
+//             ## 负责人id
+//             $leaderId = $task["leader_id"];
+//             ## 负责人姓名
+//             foreach ($userList as $user) {
+//                 if ($leaderId == $user["id"]) {
+//                     $leaderInfo = $user;
+//                 }
+//             }
+//             ## 协助人姓名
+//             foreach ($coIds as $coId) {
+//                 foreach ($userList as $user) {
+//                     if ($coId == $user["id"]) {
+//                         $coNames[] = $user;
+//                     }
+//                 }
+//             }
+//             $task["coNames"] = $coNames;
+//             $task["leaderInfo"] = $leaderInfo;
+//         }
+//         $this->assign("taskList",$taskList);
+//         echo "<pre>";
+//         print_r($taskList);
+//         print_r($userList);
+
+//         exit;
         return view();
     }
 
